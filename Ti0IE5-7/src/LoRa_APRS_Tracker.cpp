89c0f4f0bd0f743255969decbe95a9477b040ac9
JosueMV <mvhjossue56@gmail.com>
2025-05-30 19:34:12 -0600

    Codigo de boton de alerta agregado

--- Ti0IE5-7/src/LoRa_APRS_Tracker.cpp
+++ Ti0IE5-7/src/LoRa_APRS_Tracker.cpp
@@ -14,9 +14,10 @@
          ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║
          ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
 
-                       Ricardo Guzman - CA2RXU 
-          https://github.com/richonguzman/LoRa_APRS_Tracker
-             (donations : http://paypal.me/richonguzman)
+
+                                GRUPO 5  --  TEC
+            Natalia Hernández - Joham Mora - Josué Marín
+          https://github.com/JohamGab00/Taller_Integrador_Grupo_5_IS_2025
 ____________________________________________________________________*/
 
 #include <BluetoothSerial.h>
@@ -50,123 +51,154 @@ ____________________________________________________________________*/
 #include "touch_utils.h"
 #endif
 
-Configuration                       Config;
-HardwareSerial                      gpsSerial(1);
-TinyGPSPlus                         gps;
+// ===================== ZONA DE ALERTA: Definición del pin =====================
+// Pin para botón de alerta con pull-up interno
+#define ALERT_BUTTON_PIN 32
+// =============================================================================
+
+Configuration    Config;
+HardwareSerial   gpsSerial(1);
+TinyGPSPlus      gps;
 #ifdef HAS_BT_CLASSIC
-    BluetoothSerial                 SerialBT;
+BluetoothSerial   SerialBT;
 #endif
 
-String      versionDate             = "2025.03.28";
+String    versionDate       = "2025.03.28";
+uint8_t   myBeaconsIndex    = 0;
+int       myBeaconsSize     = Config.beacons.size();
+Beacon*   currentBeacon     = &Config.beacons[myBeaconsIndex];
+uint8_t   loraIndex         = 0;
+int       loraIndexSize     = Config.loraTypes.size();
+LoraType* currentLoRaType   = &Config.loraTypes[loraIndex];
 
-uint8_t     myBeaconsIndex          = 0;
-int         myBeaconsSize           = Config.beacons.size();
-Beacon      *currentBeacon          = &Config.beacons[myBeaconsIndex];
-uint8_t     loraIndex               = 0;
-int         loraIndexSize           = Config.loraTypes.size();
-LoraType    *currentLoRaType        = &Config.loraTypes[loraIndex];
+int       menuDisplay       = 100;
 
-int         menuDisplay             = 100;
+bool      statusState       = true;
+bool      displayEcoMode    = Config.display.ecoMode;
+bool      displayState      = true;
+uint32_t  displayTime       = millis();
+uint32_t  refreshDisplayTime= millis();
+bool      sendUpdate        = true;
 
-bool        statusState             = true;
-bool        displayEcoMode          = Config.display.ecoMode;
-bool        displayState            = true;
-uint32_t    displayTime             = millis();
-uint32_t    refreshDisplayTime      = millis();
+bool      bluetoothActive   = Config.bluetooth.active;
+bool      bluetoothConnected= false;
 
-bool        sendUpdate              = true;
+uint32_t  lastTx            = 0;
+uint32_t  txInterval        = 60000L;
+uint32_t  lastTxTime        = 0;
+double    lastTxLat         = 0.0;
+double    lastTxLng         = 0.0;
+double    lastTxDistance    = 0.0;
 
-bool        bluetoothActive         = Config.bluetooth.active;
-bool        bluetoothConnected      = false;
+uint32_t  menuTime          = millis();
 
-uint32_t    lastTx                  = 0.0;
-uint32_t    txInterval              = 60000L;
-uint32_t    lastTxTime              = 0;
-double      lastTxLat               = 0.0;
-double      lastTxLng               = 0.0;
-double      lastTxDistance          = 0.0;
+bool      flashlight        = false;
+bool      digipeaterActive  = false;
+bool      sosActive         = false;
 
-uint32_t    menuTime                = millis();
+bool      miceActive        = false;
 
-bool        flashlight              = false;
-bool        digipeaterActive        = false;
-bool        sosActive               = false;
+bool      smartBeaconActive = true;
 
-bool        miceActive              = false;
+int       ackRequestNumber;
+uint32_t  lastGPSTime       = 0;
 
-bool        smartBeaconActive       = true;
+APRSPacket lastReceivedPacket;
 
-int         ackRequestNumber;
-
-uint32_t    lastGPSTime             = 0;
-
-APRSPacket                          lastReceivedPacket;
-
-logging::Logger                     logger;
-//#define DEBUG
-
+logging::Logger logger;
+// #define DEBUG
 extern bool gpsIsActive;
 
 void setup() {
     Serial.begin(115200);
+#ifndef DEBUG
+    logger.setDebugLevel(logging::LoggerLevel::LOGGER_LEVEL_INFO);
+#endif
 
-    #ifndef DEBUG
-        logger.setDebugLevel(logging::LoggerLevel::LOGGER_LEVEL_INFO);
-    #endif
+    // ===================== ZONA DE ALERTA: Configurción del pin del botón =====================
+    // Configurar como pull-up interno
+    pinMode(ALERT_BUTTON_PIN, INPUT_PULLUP);
+    logger.log(logging::LoggerLevel::LOGGER_LEVEL_INFO,
+               "Main", "Botón de alerta configurado en GPIO32 con pull-up interno");
+    // =============================================================================
 
+    // Inicialización de módulos
     POWER_Utils::setup();
     displaySetup();
     POWER_Utils::externalPinSetup();
-
-    STATION_Utils::loadIndex(0);    // callsign Index
-    STATION_Utils::loadIndex(1);    // lora freq settins Index
+    STATION_Utils::loadIndex(0);
+    STATION_Utils::loadIndex(1);
     STATION_Utils::nearTrackerInit();
     startupScreen(loraIndex, versionDate);
-
     WIFI_Utils::checkIfWiFiAP();
-
     MSG_Utils::loadNumMessages();
     GPS_Utils::setup();
     currentLoRaType = &Config.loraTypes[loraIndex];
     LoRa_Utils::setup();
     Utils::i2cScannerForPeripherals();
     WX_Utils::setup();
-    
     ackRequestNumber = random(1,999);
-
     WiFi.mode(WIFI_OFF);
-    logger.log(logging::LoggerLevel::LOGGER_LEVEL_DEBUG, "Main", "WiFi controller stopped");
-
+    logger.log(logging::LoggerLevel::LOGGER_LEVEL_DEBUG,
+               "Main", "WiFi controller stopped");
     if (bluetoothActive) {
-        if (Config.bluetooth.useBLE) {
-            BLE_Utils::setup();
-        } else {
-            #ifdef HAS_BT_CLASSIC
-                BLUETOOTH_Utils::setup();
-            #endif
+        if (Config.bluetooth.useBLE) BLE_Utils::setup();
+        else {
+#ifdef HAS_BT_CLASSIC
+            BLUETOOTH_Utils::setup();
+#endif
         }
     }
-
     if (!Config.simplifiedTrackerMode) {
-        #ifdef BUTTON_PIN
-            BUTTON_Utils::setup();
-        #endif
-        #ifdef HAS_JOYSTICK
-            JOYSTICK_Utils::setup();
-        #endif
+#ifdef BUTTON_PIN
+        BUTTON_Utils::setup();
+#endif
+#ifdef HAS_JOYSTICK
+        JOYSTICK_Utils::setup();
+#endif
         KEYBOARD_Utils::setup();
-        #ifdef HAS_TOUCHSCREEN
-            TOUCH_Utils::setup();
-        #endif
+#ifdef HAS_TOUCHSCREEN
+        TOUCH_Utils::setup();
+#endif
     }
-
     POWER_Utils::lowerCpuFrequency();
-    logger.log(logging::LoggerLevel::LOGGER_LEVEL_DEBUG, "Main", "Smart Beacon is: %s", Utils::getSmartBeaconState());
-    logger.log(logging::LoggerLevel::LOGGER_LEVEL_INFO, "Main", "Setup Done!");
+    logger.log(logging::LoggerLevel::LOGGER_LEVEL_INFO,
+               "Main", "Setup Done!");
     menuDisplay = 0;
 }
 
 void loop() {
+    // ===================== ZONA DE ALERTA: Variables de estado =====================
+    static bool alertSent = false;
+    static uint32_t alertUntil = 0;
+    // =============================================================================
+
+    // ===================== ZONA DE ALERTA: Lectura del botón y envío de paquete =====================
+    bool btn = digitalRead(ALERT_BUTTON_PIN);
+    if (btn == LOW && !alertSent) {
+        alertSent = true;
+        // Construir y enviar paquete APRS de emergencia
+        String em = String(currentBeacon->callsign)
+            + ">APZTRK:!EMERGENCY ALERT!";
+        LoRa_Utils::sendNewPacket(em);
+        // Mostrar alerta en pantalla y fijar duración
+        alertUntil = millis() + 1000;
+    } else if (btn == HIGH && alertSent) {
+        // Al soltar el botón, permitir nuevo envío
+        alertSent = false;
+    }
+    // =============================================================================
+
+    // ===================== ZONA DE ALERTA: Mostrar alerta en pantalla =====================
+    if (millis() < alertUntil) {
+        displayShow("", "", "¡ALERTA APRS!",
+                    "Enviado a red APRS", "", "");
+        // Salir del loop para que no se ejecute la lógica normal mientras dure la alerta
+        return;
+    }
+    // =============================================================================
+
+    // ===================== Lógica normal GPS / menús / refresco =====================
     currentBeacon = &Config.beacons[myBeaconsIndex];
     if (statusState) {
         if (Config.validateConfigFile(currentBeacon->callsign)) {
@@ -181,24 +213,21 @@ void loop() {
     SMARTBEACON_Utils::checkState();
 
     if (!Config.simplifiedTrackerMode) {
-        #ifdef BUTTON_PIN
-            BUTTON_Utils::loop();
-        #endif
+#ifdef BUTTON_PIN
+        BUTTON_Utils::loop();
+#endif
     }
 
     Utils::checkDisplayEcoMode();
-
     KEYBOARD_Utils::read();
-    #ifdef HAS_JOYSTICK
-        JOYSTICK_Utils::loop();
-    #endif
-    #ifdef HAS_TOUCHSCREEN
-        TOUCH_Utils::loop();
-    #endif
-
+#ifdef HAS_JOYSTICK
+    JOYSTICK_Utils::loop();
+#endif
+#ifdef HAS_TOUCHSCREEN
+    TOUCH_Utils::loop();
+#endif
 
     ReceivedLoRaPacket packet = LoRa_Utils::receivePacket();
-
     MSG_Utils::checkReceivedMessage(packet);
     MSG_Utils::processOutputBuffer();
     MSG_Utils::clean15SegBuffer();
@@ -208,13 +237,13 @@ void loop() {
             BLE_Utils::sendToPhone(packet.text.substring(3));
             BLE_Utils::sendToLoRa();
         } else {
-            #ifdef HAS_BT_CLASSIC
-                BLUETOOTH_Utils::sendToPhone(packet.text.substring(3));
-                BLUETOOTH_Utils::sendToLoRa();
-            #endif
+#ifdef HAS_BT_CLASSIC
+            BLUETOOTH_Utils::sendToPhone(packet.text.substring(3));
+            BLUETOOTH_Utils::sendToLoRa();
+#endif
         }
     }
-    
+
     MSG_Utils::ledNotification();
     Utils::checkFlashlight();
     STATION_Utils::checkListenedTrackersByTimeAndDelete();
@@ -225,16 +254,12 @@ void loop() {
         bool gps_time_update = gps.time.isUpdated();
         bool gps_loc_update  = gps.location.isUpdated();
         GPS_Utils::setDateFromData();
-
-        int currentSpeed = (int) gps.speed.kmph();
-
+        int currentSpeed = (int)gps.speed.kmph();
         if (gps_loc_update) {
             Utils::checkStatus();
             STATION_Utils::checkTelemetryTx();
         }
-
         if (!gps.location.isValid()) BATTERY_Utils::checkVoltageWithoutGPSFix();
-
         if (!sendUpdate && gps_loc_update && smartBeaconActive) {
             GPS_Utils::calculateDistanceTraveled();
             if (!sendUpdate) GPS_Utils::calculateHeadingDelta(currentSpeed);
@@ -260,6 +285,5 @@ void loop() {
             refreshDisplayTime = millis();
         }
     }
+    // =============================================================================
 }
-
-// eliminar keyboardConnected y reemplazar por validar si es distinto de 0x00 ?
